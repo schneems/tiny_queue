255d7ab6d94d356d6202f5b79802ddf98b7e5737
schneems <richard.schneeman@gmail.com>
2017-08-03 16:12:41 -0500

    Ifs with no curlys scare me

--- tiny_queue.c
+++ tiny_queue.c
@@ -9,15 +9,17 @@
 tiny_queue_t* tiny_queue_create() {
   struct tiny_queue_t* queue = (struct tiny_queue_t*)malloc(sizeof(struct tiny_queue_t));
 
-  if (queue == NULL)
+  if (queue == NULL) {
     return NULL;
+  }
 
   queue->head = NULL;
   queue->tail = NULL;
 
   if(pthread_mutex_init(&queue->mutex, NULL) == 0 &&
-     pthread_cond_init(&queue->wakeup, NULL) == 0)
+     pthread_cond_init(&queue->wakeup, NULL) == 0) {
     return queue;
+  }
 
   free(queue);
   return NULL;
@@ -27,8 +29,9 @@ tiny_queue_t* tiny_queue_create() {
 int tiny_queue_push(tiny_queue_t *queue, void *x) {
   struct tiny_linked_list_t* new_node = (struct tiny_linked_list_t*)malloc(sizeof(struct tiny_linked_list_t));
 
-  if (new_node == NULL)
+  if (new_node == NULL) {
     return -1;
+  }
 
   new_node->data = x;
   new_node->next = NULL;
